2e02dcb4dd4e05645f048c14e0e8051d74044ad7
kykaroo <boris_nemcev@mail.ru>
2024-07-05 21:37:13 +0500

    Добавлен класс для хранения отдельного заказа и тесты к нему

---
+++ Store/Order.cs
@@ -0,0 +1,22 @@
+﻿namespace Store;
+
+public class Order
+{
+    public int Id { get; }
+    
+    public IReadOnlyCollection<OrderItem> Items => _items;
+
+    public int TotalCount => _items.Sum(item => item.Count);
+    public float TotalPrice => _items.Sum(item => item.Price * item.Count);
+    
+    private List<OrderItem> _items;
+
+    public Order(int id, IEnumerable<OrderItem> items)
+    {
+        ArgumentNullException.ThrowIfNull(items);
+
+        Id = id;
+        
+        _items = [..items];
+    }
+}
---
+++ Tests/OrderTests.cs
@@ -0,0 +1,53 @@
+﻿using Store;
+
+namespace Tests;
+
+public class OrderTests
+{
+    [Fact]
+    public void Order_WithNullItems_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Order(1, null!));
+    }
+    
+    [Fact]
+    public void TotalCount_WithEmptyItems_ReturnsZero()
+    {
+        var order = new Order(1, Array.Empty<OrderItem>());
+        
+        Assert.Equal(0, order.TotalCount);
+    }
+    
+    [Fact]
+    public void TotalCount_WithNonEmptyItems_CalculatesTotalCount()
+    {
+        const int firstItemCount = 3;
+        const int secondItemCount = 5;
+        
+        var order = new Order(1, new []
+        {
+            new OrderItem(1,firstItemCount,10),
+            new OrderItem(2,secondItemCount,100)
+        });
+        
+        Assert.Equal(firstItemCount + secondItemCount, order.TotalCount);
+    }
+    
+    [Fact]
+    public void TotalPrice_WithNonEmptyItems_CalculatesTotalCount()
+    {
+        const int firstItemCount = 3;
+        const int firstItemPrice = 10;
+        
+        const int secondItemCount = 5;
+        const int secondItemPrice = 100;
+        
+        var order = new Order(1, new []
+        {
+            new OrderItem(1,firstItemCount,firstItemPrice),
+            new OrderItem(2,secondItemCount,secondItemPrice)
+        });
+        
+        Assert.Equal(firstItemCount * firstItemPrice + secondItemCount * secondItemPrice, order.TotalPrice);
+    }
+}
