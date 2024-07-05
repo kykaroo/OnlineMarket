0a30dc51a0d3ca309f2ae8dbb7cd2aa7874163eb
kykaroo <boris_nemcev@mail.ru>
2024-07-05 23:02:51 +0500

    Добавлен контроллер для взаимодействия с заказом

---
+++ Web/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrderController(IProductRepository productRepository, IOrderRepository orderRepository) : Controller
+{
+    [HttpPost]
+    public IActionResult Add(int id, int numberOfItems)
+    {
+        if (numberOfItems <= 0)
+        {
+            return BadRequest("Trying to add <= 0 items to order");
+        }
+        
+        var order = orderRepository.GetById(1) ?? orderRepository.CreateOrder();
+
+        var item = productRepository.GetAllById(id.ToString());
+        
+        order.AddItem(item[0], numberOfItems);
+        orderRepository.UpdateOrder(order);
+        
+        return Ok($"Items count: {order.TotalCount}, Total price: {order.TotalPrice}");
+    }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+builder.Services.AddSingleton<IOrderRepository, OrderRepository>();
 builder.Services.AddSingleton<ProductService>();
 
 var app = builder.Build();
